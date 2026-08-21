25a80f34492c08f8558f9eda96c04201555d4ca5
rohit <rohit19801496@@gmail.com>
2026-08-21 19:03:23 +0530

    Auth.Dtos: Added Swagger ApiProperty to set the example

--- backend/src/auth/dto/login.dto.ts
+++ backend/src/auth/dto/login.dto.ts
@@ -1,10 +1,13 @@
+import { ApiProperty } from "@nestjs/swagger";
 import { IsEmail, IsString, MinLength } from "class-validator";
 
 export class LoginDto {
   @IsEmail()
+  @ApiProperty({example: "yourname@service.com"})
   email!: string;
 
   @IsString()
   @MinLength(8)
+  @ApiProperty({example: "yourPassword@123"})
   password!: string;
 }
